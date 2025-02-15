7d3edaaa441a4ccefc2896e5362c636006a175a8
shan4smiles-2 <shan.pluralsight.unofficial@gmail.com>
2025-02-16 01:06:45 +0530

    Update streamlit_app.py
    Debugging error with Parsing REST API code to dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,4 +74,4 @@ import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response) # simple: "Response <200>"
 st.text(smoothiefroot_response.json()) # modify the format: "json object"
-st_df = st.dataframe(data = smoothiefroot_response.json, use_container_width = True)
+st_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
