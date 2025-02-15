351867166116a62b7e777aff0824b610e3d3c31b
shan4smiles-2 <shan.pluralsight.unofficial@gmail.com>
2025-02-16 01:05:42 +0530

    Update streamlit_app.py
    Parsing REST API response 200 to json and dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,4 +72,6 @@ if ingredients_list: # to show [] only when we select
 # Adding a RESTful API connection - SMOOTHIE nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response) # simple: "Response <200>"
+st.text(smoothiefroot_response.json()) # modify the format: "json object"
+st_df = st.dataframe(data = smoothiefroot_response.json, use_container_width = True)
